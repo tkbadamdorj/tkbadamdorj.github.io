abee6721a95d5382473375d907d8563694a952a5
tkbadamdorj <tkbadamdorj@gmail.com>
2025-03-28 03:32:18 +0000

    Deploying to gh-pages from @ tkbadamdorj/tkbadamdorj.github.io@bc36eefb4110e775df41644f1a71853f26f84b4f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,6 +34,17 @@ ninja.data = [{
           window.location.href = "/blog/2025/presentations/";
         
       },
+    },{id: "post-training-foundation-models-up-to-10x-more-efficiently-with-memory-mapped-datasets",
+      
+        title: 'Training foundation models up to 10x more efficiently with Memory-Mapped Datasets <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.open("https://rbcborealis.com/research-blogs/training-foundation-models-10x-more-efficiently-with-memory-mapped-datasets/", "_blank");
+        
+      },
     },{id: "post-going-from-nothing-to-a-first-model",
       
         title: "Going from nothing to a first model",
