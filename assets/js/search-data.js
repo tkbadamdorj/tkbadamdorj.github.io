1b406de34e9f9799c8586d60b8c703e441d714ae
tkbadamdorj <tkbadamdorj@gmail.com>
2025-04-06 19:01:58 +0000

    Deploying to gh-pages from @ tkbadamdorj/tkbadamdorj.github.io@5fd3355ae95d9c2f371a5317bcdba6a9096e178e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -47,7 +47,7 @@ ninja.data = [{
       },
     },{id: "post-training-foundation-models-up-to-10x-more-efficiently-with-memory-mapped-datasets",
       
-        title: 'Training foundation models up to 10x more efficiently with Memory-Mapped Datasets <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        title: 'Training foundation models up to 10x more efficiently with memory-mapped datasets <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
       description: "",
       section: "Posts",
